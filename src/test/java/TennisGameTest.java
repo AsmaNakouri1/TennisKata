a282cda07c8b73fbd68c792c4d497d2a0daf58c4
Asma Nakouri <Nakouri.asma@yahoo.fr>
2019-02-18 15:54:16 +0100

    refactor Test

--- src/test/java/TennisGameTest.java
+++ src/test/java/TennisGameTest.java
@@ -1,8 +1,11 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@RunWith(JUnitParamsRunner.class)
 public class TennisGameTest {
 
     @Test
@@ -13,73 +16,95 @@ public class TennisGameTest {
     }
 
     @Test
-    public void should_display_fifteen_to_zero_as_game_score_and_zero_zero_as_set_score_when_the_first_player_wins_the_first_point(){
+    @Parameters({
+            "0, 0, 0 _ 0",
+            "1, 0, 15 _ 0",
+            "2, 0, 30 _ 0",
+            "2, 1, 30 _ 15",
+            "3, 1, 40 _ 15",
+            "3, 2, 40 _ 30",
+            "3, 3, 40 _ 40",
+            "0, 3, 0 _ 40"
+    })
+    public void should_display_right_when_the_number_of_each_player_wins_is_lower_then_3(int firstPlayerWins, int secondPlayerWins, String gameScore) {
+
         TennisGame tennisGame = new TennisGame();
-        tennisGame.firstPlayerWinsOnePoint();
-        assertThat(tennisGame.getScoreGame()).isEqualTo("15 _ 0");
+
+        updatePlayersWins(firstPlayerWins,secondPlayerWins,tennisGame);
+
+        assertThat(tennisGame.getScoreGame()).isEqualTo(gameScore);
+
     }
 
-    @Test
-    public void should_display_thirty_to_fifteen_when_the_first_player_wins_tow_points_and_the_second_player_wins_one_point(){
-        TennisGame tennisGame = new TennisGame();
-        tennisGame.firstPlayerWinsOnePoint();
-        tennisGame.firstPlayerWinsOnePoint();
-        tennisGame.secondPlayerWinsOnePoint();
-        assertThat(tennisGame.getScoreGame()).isEqualTo("30 _ 15");
-    }
 
     @Test
-    public void should_be_added_10_to_the_score_game_when_the_first_player_wins_one_point_and_his_score_is_30_and_the_second_player_wins_one_point(){
-        TennisGame tennisGame = new TennisGame();
-        tennisGame.firstPlayerWinsOnePoint();
-        tennisGame.firstPlayerWinsOnePoint();
-        tennisGame.secondPlayerWinsOnePoint();
-        tennisGame.firstPlayerWinsOnePoint();
-        assertThat(tennisGame.getScoreGame()).isEqualTo("40 _ 15");
-    }
+    @Parameters({
+            "1, 3, 0",
+            "1, 3, 1",
+            "1, 3, 2",
+            "2, 0, 3",
+            "2, 1, 3",
+            "2, 2, 3",
 
-    @Test
-    public void should_display_forty_to_forty_when_the_first_and_the_second_player_win_3_point_each_one(){
-        TennisGame tennisGame = new TennisGame();
-        tennisGame.firstPlayerWinsOnePoint();
-        tennisGame.firstPlayerWinsOnePoint();
-        tennisGame.secondPlayerWinsOnePoint();
-        tennisGame.firstPlayerWinsOnePoint();
-        tennisGame.secondPlayerWinsOnePoint();
-        tennisGame.secondPlayerWinsOnePoint();
-        assertThat(tennisGame.getScoreGame()).isEqualTo("40 _ 40");
-    }
-
-    @Test
-    public void should_display_zero_to_zero_when_the_score_was_40_30_and_player_tow_wins_the_point_and_wins_the_game(){
+    })
+    public void should_display_zero_zero_as_game_score_when_a_player_wins_4_points_and_has_tow_more_points_than_the_other(int player, int firstPlayerWins, int secondPlayerWins) {
         TennisGame tennisGame = new TennisGame();
 
-        updatePlayersWins(2,3,tennisGame);
-        
-        tennisGame.secondPlayerWinsOnePoint();
+        updatePlayersWins(firstPlayerWins,secondPlayerWins,tennisGame);
+
+        playerWinsOnePoint(player, tennisGame);
+
         assertThat(tennisGame.getScoreGame()).isEqualTo("0 _ 0");
     }
 
+
     @Test
-    public void should_display_ADV_40_as_score_game_when_the_first_player_wins_one_point_and_the_score_was_40_40() {
+    @Parameters({
+            "1 , ADV _ 40",
+            "2 , 40 _ ADV",
+    })
+    public void should_display_ADV_for_the_player_how_wins_one_point_and_the_score_was_40_40(int player, String gameScore) {
         TennisGame tennisGame = new TennisGame();
 
         updatePlayersWins(3,3,tennisGame);
 
-        tennisGame.firstPlayerWinsOnePoint();
+        playerWinsOnePoint(player, tennisGame);
 
-        assertThat(tennisGame.getScoreGame()).isEqualTo("ADV _ 40");
+        assertThat(tennisGame.getScoreGame()).isEqualTo(gameScore);
+    }
+
+
+    @Test
+    @Parameters({
+            "1 , 3, 4",
+            "2 , 4, 3"
+    })
+    public void should_display_DEUCE_Deuce_when_the_player_how_has_40_as_score_wins_one_point_and_the_score_was_40_to_ADV(int player, int firstPlayerWins, int secondPlayerWins) {
+        TennisGame tennisGame = new TennisGame();
+
+        updatePlayersWins(firstPlayerWins, secondPlayerWins,tennisGame);
+
+        playerWinsOnePoint(player, tennisGame);
+
+        assertThat(tennisGame.getScoreGame()).isEqualTo("DEUCE _ DEUCE");
     }
 
     @Test
-    public void should_display_40_ADV_as_score_game_when_the_second_player_wins_one_point_and_the_score_was_40_40() {
+    @Parameters({
+            "1, 4, 3",
+            "2, 3, 4",
+            "2, 4, 5",
+            "1, 5, 4"
+    })
+    public void should_display_zero_zero_as_game_score_when_the_tow_players_win_more_than_three_points_and_the_difference_between_the_number_of_points_is_equal_to_2(int player, int firstPlayerWins, int secondPlayerWins) {
         TennisGame tennisGame = new TennisGame();
 
-        updatePlayersWins(3,3,tennisGame);
+        updatePlayersWins(firstPlayerWins,secondPlayerWins,tennisGame);
 
-        tennisGame.secondPlayerWinsOnePoint();
+        playerWinsOnePoint(player,tennisGame);
 
-        assertThat(tennisGame.getScoreGame()).isEqualTo("40 _ ADV");
+        assertThat(tennisGame.getScoreGame()).isEqualTo("0 _ 0");
+
     }
 
     private void updatePlayersWins(int firstPlayerWins, int secondPlayerWins, TennisGame tennisGame){
@@ -90,25 +115,9 @@ public class TennisGameTest {
             tennisGame.secondPlayerWinsOnePoint();
     }
 
-    @Test
-    public void should_display_DEUCE_to_DEUCE_when_the_score_is_40_TO_ADV_and_the_first_player_wins_one_point() {
-        TennisGame tennisGame = new TennisGame();
-
-        updatePlayersWins(3,4,tennisGame);
-
-        tennisGame.firstPlayerWinsOnePoint();
-
-        assertThat(tennisGame.getScoreGame()).isEqualTo("DEUCE _ DEUCE");
+    private void playerWinsOnePoint(int player, TennisGame tennisGame) {
+        if(player ==1)  tennisGame.firstPlayerWinsOnePoint();
+        if(player ==2)  tennisGame.secondPlayerWinsOnePoint();
     }
 
-    @Test
-    public void should_display_DEUCE_to_DEUCE_when_the_score_is_ADV_40_and_the_second_player_wins_one_point() {
-        TennisGame tennisGame = new TennisGame();
-
-        updatePlayersWins(4,3,tennisGame);
-
-        tennisGame.secondPlayerWinsOnePoint();
-
-        assertThat(tennisGame.getScoreGame()).isEqualTo("DEUCE _ DEUCE");
-    }
 }
